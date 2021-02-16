ab37295d253a32cd43c7f32c94b6b4c9f14a67b5
Jeff Schornick <jeff@schornick.org>
2021-02-16 04:38:02 -0700

    Index WaveProducts by model number instead of name

--- wave_reader/data.py
+++ wave_reader/data.py
@@ -2,15 +2,22 @@ from enum import Enum
 
 
 class WaveProduct(Enum):
-    WAVEPLUS = "Airthings Wave+"
-    WAVE = "Airthings Wave"
-    WAVEMINI = "Airthings Wave Mini"
+    WAVE = '2900'
+    WAVEMINI = '2920'
+    WAVEPLUS = '2930'
+    WAVE2 = '2950'
 
 
-IDENTITY = 820
-SERIAL_NUMBER_BUFFER = "<LH"
+# Company Identifier registered with the Bluetooth SIG
+# Officially registered as "Corentium AS"
+AIRTHINGS_ID = 820
+
+# Model and serial number
+MANUFACTURER_DATA_FORMAT = "<LH"
+
 DEVICE = {
     WaveProduct.WAVEPLUS: {
+        "name": "Wave+",
         "UUID": "b42e2a68-ade7-11e4-89d3-123b93f75cba",
         "BUFFER": "<BBBBHHHHHHHH",
         "SENSOR_FORMAT": (
@@ -26,6 +33,20 @@ DEVICE = {
         ),
     },
     WaveProduct.WAVE: {
+        "name": "Wave",
+        "UUID": "b42e4dcc-ade7-11e4-89d3-123b93f75cba",
+        "BUFFER": "<4B8H",
+        "SENSOR_FORMAT": (
+            lambda d: {
+                "humidity": d[1] / 2.0 if d[1] else 0,
+                "radon_sta": d[4],
+                "radon_lta": d[5],
+                "temperature": d[6] / 100.0 if d[6] else 0,
+            }
+        ),
+    },
+    WaveProduct.WAVE2: {
+        "name": "Wave (2nd Gen)",
         "UUID": "b42e4dcc-ade7-11e4-89d3-123b93f75cba",
         "BUFFER": "<4B8H",
         "SENSOR_FORMAT": (
@@ -38,6 +59,7 @@ DEVICE = {
         ),
     },
     WaveProduct.WAVEMINI: {
+        "name": "Wave Mini",
         "UUID": "b42e3b98-ade7-11e4-89d3-123b93f75cba",
         "BUFFER": "<HHHHHHLL",
         "SENSOR_FORMAT": (
