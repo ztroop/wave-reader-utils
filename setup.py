629532c70f2d81f254ecaeaa1438aeb8b1537e36
Zackary Troop <zack.tro@gmail.com>
2022-12-15 22:32:38 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     author="Zackary Troop",
     name="wave-reader",
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/ztroop/wave-reader-utils",
     license="MIT",
     description="Unofficial package for Airthings Wave communication.",
