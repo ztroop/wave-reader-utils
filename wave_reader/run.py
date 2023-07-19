766b3d4ecb2ce5b15db8764c36dfbf0c44243c21
Zackary Troop <zack.tro@gmail.com>
2023-07-18 20:29:06 -0400

    Add run script

---
+++ wave_reader/run.py
@@ -0,0 +1,18 @@
+import asyncio
+import logging
+from wave_reader import discover_devices, wave
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def get_readings(device: wave.WaveDevice) -> None:
+    sensor_readings = await device.get_sensor_values()
+    print(sensor_readings)
+    await device.disconnect()
+
+
+if __name__ == "__main__":
+    loop = asyncio.new_event_loop()
+    devices = loop.run_until_complete(discover_devices(timeout=5.0))
+    for device in devices:
+        loop.run_until_complete(get_readings(device))
